5e6e956249661e6c82d42fa7571316ca0eaefd9e
Aleksey Likhodievski <aleksey.likhodievski@gmail.com>
2022-10-03 17:21:24 +0300

    Updates

---
+++ static/js/14.28ed64e3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfake_store=self.webpackChunkfake_store||[]).push([[14],{8014:function(t,e,a){a.d(e,{vHB:function(){return r}});var n=a(9983);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"}}]})(t)}}}]);
+//# sourceMappingURL=14.28ed64e3.chunk.js.map
